b8f6af5957490536ee0bb210645f47c51ce40c96
colom <colom@DARIO>
2023-10-18 20:59:23 -0500

    Agregar archivos de proyecto.

---
+++ ServicioEmpleado/IServicioEmpleado.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Servicios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioEmpleado
+    {
+        [OperationContract]
+        Boolean ActualizarEmpleado(EmpleadoDC empleado);
+        
+        [OperationContract]
+        Boolean InsertarEmpleado(EmpleadoDC empleado);
+        
+        [OperationContract]
+        Boolean EliminarEmpleado(string strCod);
+
+
+        [OperationContract]
+        EmpleadoDC ConsultarEmpleado(String strCod);
+
+        [OperationContract]
+        List<EmpleadoDC> ListarEmpleado();
+
+
+
+        // TODO: agregue aquí sus operaciones de servicio
+    }
+
+    // Utilice un contrato de datos, como se ilustra en el ejemplo siguiente, para agregar tipos compuestos a las operaciones de servicio.
+    // Puede agregar archivos XSD al proyecto. Después de compilar el proyecto, puede usar directamente los tipos de datos definidos aquí, con el espacio de nombres "ServicioEmpleado.ContractType".
+    [Serializable]
+    [DataContract]
+    public class EmpleadoDC
+    {
+
+        [DataMember]
+
+        public string IDEmpleado { get; set; }
+
+        [DataMember]
+
+        public string IDUbigeo { get; set; }
+
+        [DataMember]
+        public string Nom_empl { get; set; }
+
+        [DataMember]
+        public string ape_empl { get; set; }
+        
+        [DataMember]
+        public DateTime fecha_ingreso_empl { get; set; }
+        
+        [DataMember]
+        public string telefono_empl { get; set; }
+        
+        [DataMember]
+        public string correo_empl { get; set; }
+
+
+        [DataMember]
+        public DateTime fec_nacimiento_empl { get; set; }
+
+
+        [DataMember]
+        public DateTime fec_vencimi_contr_empl { get; set; }
+
+
+        [DataMember]
+        public string sexo_empl { get; set; }
+
+
+        [DataMember]
+        public string dni_empl { get; set; }
+
+
+        [DataMember]
+        public DateTime fec_reg_empl { get; set; }
+
+
+        [DataMember]
+        public string usu_reg_empl { get; set; }
+
+
+        [DataMember]
+        public DateTime fec_ultm_mod_empl { get; set; }
+
+
+        [DataMember]
+        public string usu_ultm_mod_empl { get; set; }
+
+
+        [DataMember]
+        public string estado_empl { get; set; }
+
+
+        [DataMember]
+        public byte[] Foto { get; set; }
+
+
+        [DataMember]
+        public string id_ACF { get; set; }
+
+
+    }
+}
---
+++ ServicioEmpleado/ServicioEmpleado.cs
@@ -0,0 +1,160 @@
+﻿using ServicioEmpleado;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Servicios
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "Service1" en el código y en el archivo de configuración a la vez.
+    public class ServicioEmpleado : IServicioEmpleado
+    {
+        public bool ActualizarEmpleado(EmpleadoDC empleado)
+        {
+            try
+            {
+                CerveceriaEntities cerveceria = new CerveceriaEntities();
+
+
+                cerveceria.usp_ActualizarEmpleado(
+                    empleado.IDEmpleado, empleado.IDUbigeo, empleado.Nom_empl, empleado.ape_empl, empleado.telefono_empl,
+                    empleado.correo_empl, empleado.fec_vencimi_contr_empl, empleado.sexo_empl, empleado.dni_empl, empleado.usu_reg_empl,
+                    empleado.estado_empl, empleado.id_ACF
+                    );
+
+                cerveceria.SaveChanges();
+
+                return true;
+            } 
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public EmpleadoDC ConsultarEmpleado(string strCod)
+        {
+            try
+            {
+                CerveceriaEntities cerveceria = new CerveceriaEntities();
+
+                EmpleadoDC empleadoDC = new EmpleadoDC();
+
+                TB_Empleado tb_Empleado = (
+                    (from miEmpleado in cerveceria.TB_Empleado where miEmpleado.IDEmpleado == strCod select miEmpleado).FirstOrDefault()
+                    );
+
+                empleadoDC.IDEmpleado = tb_Empleado.IDEmpleado;
+                empleadoDC.IDUbigeo = tb_Empleado.IDUbigeo;
+                empleadoDC.Nom_empl = tb_Empleado.Nom_empl;
+                empleadoDC.ape_empl = tb_Empleado.ape_empl;
+                empleadoDC.fecha_ingreso_empl = Convert.ToDateTime(tb_Empleado.fecha_ingreso_empl);
+                empleadoDC.telefono_empl = tb_Empleado.telefono_empl;
+                empleadoDC.correo_empl = tb_Empleado.correo_empl;
+                empleadoDC.fec_nacimiento_empl = Convert.ToDateTime(tb_Empleado.fec_nacimiento_empl);
+                empleadoDC.fec_vencimi_contr_empl = Convert.ToDateTime(tb_Empleado.fec_vencimi_contr_empl);
+                empleadoDC.sexo_empl = tb_Empleado.sexo_empl;
+                empleadoDC.dni_empl = tb_Empleado.dni_empl;
+                empleadoDC.fec_reg_empl = Convert.ToDateTime(tb_Empleado.fec_reg_empl);
+                empleadoDC.usu_reg_empl = tb_Empleado.usu_reg_empl;
+                empleadoDC.fec_ultm_mod_empl = Convert.ToDateTime(tb_Empleado.fec_reg_empl);
+                empleadoDC.usu_ultm_mod_empl = tb_Empleado.usu_ultm_mod_empl;
+                empleadoDC.estado_empl = tb_Empleado.estado_empl;
+                empleadoDC.Foto = tb_Empleado.Foto;
+                empleadoDC.id_ACF = tb_Empleado.id_ACF;
+
+
+
+                return empleadoDC;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool EliminarEmpleado(string strCod)
+        {
+            try
+            {
+                CerveceriaEntities cerveceria = new CerveceriaEntities();
+
+                cerveceria.usp_EliminarEmpleado(strCod);
+
+                cerveceria.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public bool InsertarEmpleado(EmpleadoDC empleado)
+        {
+            try
+            {
+                CerveceriaEntities cerveceria = new CerveceriaEntities();
+
+                cerveceria.usp_InsertarEmpleado(empleado.IDUbigeo, empleado.Nom_empl, empleado.ape_empl,
+                    empleado.fecha_ingreso_empl, empleado.telefono_empl, empleado.correo_empl, empleado.fec_nacimiento_empl, empleado.fec_vencimi_contr_empl,
+                    empleado.sexo_empl, empleado.dni_empl, empleado.usu_reg_empl, empleado.estado_empl, empleado.Foto, empleado.id_ACF);
+
+                cerveceria.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<EmpleadoDC> ListarEmpleado()
+        {
+            try
+            {
+                CerveceriaEntities cerveceria = new CerveceriaEntities();
+
+                List<EmpleadoDC> empleados = new List<EmpleadoDC>();
+
+                var query = (from miEmpleado in cerveceria.TB_Empleado orderby miEmpleado.ape_empl select miEmpleado).ToList();
+
+                foreach (var result in query)
+                {
+                    EmpleadoDC empleadoDC = new EmpleadoDC();
+
+                    empleadoDC.IDEmpleado = result.IDEmpleado;
+                    empleadoDC.IDUbigeo = result.IDUbigeo;
+                    empleadoDC.Nom_empl = result.Nom_empl;
+                    empleadoDC.ape_empl = result.ape_empl;
+                    empleadoDC.fecha_ingreso_empl = Convert.ToDateTime(result.fecha_ingreso_empl);
+                    empleadoDC.telefono_empl = result.telefono_empl;
+                    empleadoDC.correo_empl = result.correo_empl;
+                    empleadoDC.fec_nacimiento_empl = Convert.ToDateTime(result.fec_nacimiento_empl);
+                    empleadoDC.fec_vencimi_contr_empl = Convert.ToDateTime(result.fec_vencimi_contr_empl);
+                    empleadoDC.sexo_empl = result.sexo_empl;
+                    empleadoDC.dni_empl = result.dni_empl;
+                    empleadoDC.fec_reg_empl = Convert.ToDateTime(result.fec_reg_empl);
+                    empleadoDC.usu_reg_empl = result.usu_reg_empl;
+                    empleadoDC.fec_ultm_mod_empl = Convert.ToDateTime(result.fec_reg_empl);
+                    empleadoDC.usu_ultm_mod_empl = result.usu_ultm_mod_empl;
+                    empleadoDC.estado_empl = result.estado_empl;
+                    //empleadoDC.Foto = result.Foto;
+                    empleadoDC.id_ACF = result.id_ACF;
+
+                    empleados.Add(empleadoDC);
+                }
+
+                return empleados;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
